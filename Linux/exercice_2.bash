b66bfb882f0e736eba0ef22de3356b302ef78f8f
PearlClone <benjaminlecomte37@gmail.com>
2023-01-27 16:55:18 +0100

    update

---
+++ Linux/exercice_2.bash
@@ -0,0 +1,8 @@
+mkdir Unix
+mkdir Python
+mkdir Python/TP1
+mkdir Python/TP2
+mkdir Unix/TP1
+mkdir Unix/TP2
+mkdir Unix/TP1/seance1
+mkdir Unix/TP1/seance2
---
+++ Linux/programmes/bonjour.py
@@ -0,0 +1,4 @@
+from time import sleep
+nom = input("Quel est votre nom ? ")
+print("bonjour ", nom)
+sleep(5)
